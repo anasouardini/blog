3b48be542e9c5e9cf89e6c0b2eb69e82622ab5a8
segfaulty1 <segfaulty@proton.me>
2023-07-19 17:01:53 +0100

    enhancemnt - DX: made the jsonld fixing script more generic.

--- fixJsonLD.js
+++ fixJsonLD.js
@@ -1,27 +1,32 @@
 import fs from 'fs';
 
-const filePath = './dist/index.html';
+const filePathList = ['./dist/index.html', './dist/privacy-policy/index.html', './dist/posts/index.html'];
 
-let htmlString = fs.readFileSync(filePath, 'utf-8');
+const fixJsonLD = (file)=>{
+    let htmlString = fs.readFileSync(file, 'utf-8');
 
-// replacing p with script
-htmlString = htmlString.replace(/<p.*?(type=?application\/ld\+json?)/, '<script $1');
-const chunks = htmlString.split('application/ld+json');
-chunks[1] = chunks[1].replace(/<\/p>/, '</script>');
-htmlString = chunks.join('application/ld+json');
+    // replacing p with script
+    htmlString = htmlString.replace(/<p.*?(type=?application\/ld\+json?)/, '<script $1');
+    const chunks = htmlString.split('application/ld+json');
+    chunks[1] = chunks[1].replace(/<\/p>/, '</script>');
+    htmlString = chunks.join('application/ld+json');
 
-// extracting the json+ld
-const jsonldscriptpattern = /<script type=?application\/ld\+json.*?<\/script>/;
-let extractedJsonLD = htmlString.match(jsonldscriptpattern);
-if(!extractedJsonLD){
-    process.exit(1);
+    // extracting the json+ld
+    const jsonldscriptpattern = /<script type=?application\/ld\+json.*?<\/script>/;
+    let extractedJsonLD = htmlString.match(jsonldscriptpattern);
+    if(!extractedJsonLD){
+        process.exit(1);
+    }
+
+    // deleting the json+ld
+    htmlString = htmlString.replace(jsonldscriptpattern, '');
+
+    const chunks2 = htmlString.split('</head>');
+    chunks2[0] = chunks2[0] +''+ extractedJsonLD[0].replace(/&quot;/g, '"');
+    htmlString = chunks2.join('</head>')
+
+    fs.writeFileSync(file, htmlString);
 }
-
-// deleting the json+ld
-htmlString = htmlString.replace(jsonldscriptpattern, '');
-
-const chunks2 = htmlString.split('</head>');
-chunks2[0] = chunks2[0] +''+ extractedJsonLD[0].replace(/&quot;/g, '"');
-htmlString = chunks2.join('</head>')
-
-fs.writeFileSync(filePath, htmlString);
+filePathList.forEach((file)=>{
+    fixJsonLD(file);
+});
