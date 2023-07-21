f975885b2b58e6aa7ce4d4ae7733e401fee830ae
segfaulty1 <segfaulty@proton.me>
2023-07-21 21:57:35 +0100

    enhancement - ux: abstracted some logic from the pages components.

--- src/tools.ts
+++ src/tools.ts
@@ -3,10 +3,10 @@ function slugify(str) {
     .normalize('NFKD') // split accented characters into their base characters and diacritical marks
     .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
     .trim() // trim leading or trailing whitespace
-    .toLowerCase() // convert to lowercase
     .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
     .replace(/\s+/g, '-') // replace spaces with hyphens
-    .replace(/-+/g, '-'); // remove consecutive hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
+    .toLowerCase() // convert to lowercase
 }
 
 const strCount = (str, target)=>{
@@ -15,4 +15,18 @@ const strCount = (str, target)=>{
   return smallCaseTarget.split(smallCaseStr).length - 1;
 }
 
-export default {slugify, strCount}
+const randID = (length:number)=>{
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+
+}
+
+export default {slugify, strCount, randID}
